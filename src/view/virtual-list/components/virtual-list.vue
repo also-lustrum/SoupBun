6a3ac4aea6a20abcbddc340c67b282d033d458df
tangshujie <3131923090@qq.com>
2023-08-10 17:58:09 +0800

    create virtual list

---
+++ src/view/virtual-list/components/virtual-list.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="list-container">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  // 每行元素高度
+  itemHeight: {
+    type: Number,
+    default: 160
+  },
+  // 数据总数
+  total: {
+    type: Number,
+    required: true
+  },
+  // 每屏显示条数
+  size: {
+    type: Number,
+    default: 10
+  },
+  // 预加载n屏数据
+  page: {
+    type: Number,
+    default: 1
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.container {
+  overflow-x: hidden;
+  overflow-y: auto;
+  width: 100%;
+  height: 100%;
+  border: 1px solid blue;
+}
+.list-container {
+  width: 100%;
+}
+</style>
--- src/view/virtual-list/index.vue
+++ src/view/virtual-list/index.vue
@@ -1,7 +1,37 @@
 <template>
-  <div></div>
+  <div class="page">
+    <virtual-list :total="list.length">
+      <div class="v-list-item" v-for="item in list" :key="item.id">
+        {{ item.name }}
+      </div>
+    </virtual-list>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import virtualList from './components/virtual-list.vue'
 
-<style lang="scss" scoped></style>
+interface List {
+  id: number
+  name: string
+}
+const list = ref<List[]>([])
+const getList = () => {
+  for (let i = 1; i <= 100; i++) {
+    list.value.push({
+      id: i,
+      name: `i is ${i}`
+    })
+  }
+}
+onMounted(() => {
+  getList()
+})
+</script>
+
+<style lang="scss" scoped>
+.page {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
