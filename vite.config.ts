c2c488aea16ddddece2a12b498a2e379033b3c3f
tangshujie <3131923090@qq.com>
2023-08-10 11:43:59 +0800

    配置 vue  自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue'],
+  })],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
