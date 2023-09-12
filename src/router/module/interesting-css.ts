f825fc323b5c1678e561fc2b7082c431f8ffa798
tangshujie <3131923090@qq.com>
2023-09-12 13:44:57 +0800

    网格布局动画

--- src/router/module/interesting-css.ts
+++ src/router/module/interesting-css.ts
@@ -14,5 +14,10 @@ export const interestingCss = [
     // 文字交融展开
     path: '/text-merge',
     component: () => import('@/view/interesting-css/text-merge/index.vue')
+  },
+  {
+    // 网格布局动画
+    path: '/grid-animation',
+    component: () => import('@/view/interesting-css/grid-animation/index.vue')
   }
 ]
---
+++ src/view/interesting-css/grid-animation/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="page">
+    <div class="container">
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.page {
+  overflow: hidden;
+  width: 100%;
+  height: 100vh;
+  background-color: #23262d;
+}
+.container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  gap: 10px;
+  width: 400px;
+  height: 400px;
+  margin: 50px auto 0;
+  transition: all 1s;
+}
+
+@for $i from 0 to 9 {
+  .item:nth-child(#{$i + 1}) {
+    background: hsl($i * 40%, 100%, 74%);
+  }
+  .container:has(.item:nth-child(#{$i + 1}):hover) {
+    $r: floor($i / 3 + 1);
+    $c: $i % 3 + 1;
+    $arr: 1fr 1fr 1fr;
+    $rows: set-nth($arr, $r, 2fr);
+    $columns: set-nth($arr, $c, 2fr);
+    grid-template-columns: $columns;
+    grid-template-rows: $rows;
+  }
+}
+</style>
